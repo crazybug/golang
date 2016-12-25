79f42652e59e7f040f5c29eef47d4521d12edd99
crazybug <viewit@163.com>
2016-12-25 22:00:11 +0800

    add new func,add and mul

--- hello/hello.go
+++ hello/hello.go
@@ -5,7 +5,16 @@ import (
          "github.com/crazybug/stringutil"
 )
 
+func add(x, y int) int {
+    return x + y
+}
+
+func mul(x, y int) int {
+    return x*y
+}
 
 func main() {
     fmt.Println(stringutil.Reverse("!oG ,olleH"))
+    fmt.Println(add(100,99))
+    fmt.Println(mul(100,99))
 }
