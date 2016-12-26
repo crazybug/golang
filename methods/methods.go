fc012599ae7b9f716f08a1a391654108b044978d
crazybug <viewit@163.com>
2016-12-26 21:25:52 +0800

    add Abs_func for methods

--- methods/methods.go
+++ methods/methods.go
@@ -13,7 +13,12 @@ func (v Vertex) Abs() float64 {
     return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
 
+func Abs_func(v Vertex) float64 {
+    return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
 func main() {
     v := Vertex{3, 4}
     fmt.Println(v.Abs())
+    fmt.Println(Abs_func(v))
 }
