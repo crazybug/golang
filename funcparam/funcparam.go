f6dc5476d69f7ad68905f5eb1dd7778b16c9d5e4
crazybug <viewit@163.com>
2016-12-31 16:49:02 +0800

    add a param of function type

---
+++ funcparam/funcparam.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+type testint func(int) bool //声明一个函数类型
+
+func isOdd(integer int) bool {
+	if integer%2 == 0 {
+		return false
+	}
+	return true
+}
+
+func isEven(integer int) bool {
+	if integer%2 == 0 {
+		return true
+	}
+	return false
+}
+
+//定义一个参数为函数类型的函数
+func filter(slice []int, f testint) []int {
+	var result []int
+	for _, value := range slice {
+		if f(value) {
+			result = append(result, value)
+		}
+	}
+	return result
+}
+
+func main() {
+	slice := []int{1, 2, 3, 4, 5, 6, 7}
+	fmt.Println("slice = ", slice)
+	odd := filter(slice, isOdd)
+	fmt.Println("Odd elements of slice are: ", odd)
+	even := filter(slice, isEven)
+	fmt.Println("Even elements of slice are: ", even)
+}
