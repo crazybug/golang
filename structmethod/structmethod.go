7f062d158106926e16d0c4fd9dc62b55f0b94c84
crazybug <viewit@163.com>
2017-01-01 23:39:44 +0800

    add method for build-in type

--- structmethod/structmethod.go
+++ structmethod/structmethod.go
@@ -21,11 +21,25 @@ func (r Circle) area() float64 {
 	return r.radius * r.radius * math.Pi
 }
 
+type MySlice []string
+
+func (s MySlice) toString() string {
+	var x string = ""
+	for _, v := range s {
+		x += v
+	}
+	return x
+}
+
 func main() {
 	r1 := Rectangle{12, 5}
 	c1 := Circle{10}
 
 	fmt.Println("Area of r1 is :", r1.area())
 	fmt.Println("Area of c1 is :", c1.area())
+
+	myslice := MySlice{"a", "b", "c", "d"}
+	fmt.Println(myslice)
+	fmt.Println(myslice.toString())
 
 }
